3bad0963f373c672be3a69df95dad52dfab79211
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 18:21:55 +0200

    update23

--- Project_sitomassaggi/Backend/API/Models/database.php
+++ Project_sitomassaggi/Backend/API/Models/database.php
@@ -2,6 +2,8 @@
 
 namespace API\Models;
 
+use PDO;
+
 class database extends \PDO
 {
     private static $istanza;
@@ -14,7 +16,7 @@ class database extends \PDO
         $username = getenv('DB_USER');
         $password = getenv('DB_PASSWORD');
         try {
-            $this->conn = $this->connect("pgsql:host=$serverName;port=17864;dbname=$database;user=$username;password=$password;sslmode=verify-ca;sslrootcert=./ca.pem", $username, $password);
+            $this->conn= new PDO("pgsql:host=$serverName;port=17864;dbname=$database;user=$username;password=$password;sslmode=verify-ca;sslrootcert=./ca.pem");
             $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             http_response_code(500);
