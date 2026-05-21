dadef708540de960178915696edcd427904182b4
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 10:50:38 +0200

    hotfix

--- Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
+++ Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
@@ -1,10 +1,11 @@
 import styles from "./errorcomponent.module.css"
+import sadFace from "../../../public/Images/sad.png"
 
 export default function ErrorComponent(){
     return (
         <div className={styles.errorMessage}> 
             Non è stato possibile ricavare i massaggi, riprovare più tardi
-            <img src="/images/sad.png" style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
+            <img src={sadFace} style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
         </div>
     )
 }
