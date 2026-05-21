2778c1439aa90e72e5fed7a43ac73faadc90e1a1
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 11:09:44 +0200

    update1

--- Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
+++ Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
@@ -4,7 +4,7 @@ export default function ErrorComponent(){
     return (
         <div className={styles.errorMessage}> 
             Non è stato possibile ricavare i massaggi, riprovare più tardi
-            <img src="%PUBLIC_URL%/images/sad.png" style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
+            <img src="/images/sad.png" style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
         </div>
     )
 }
