d472e6dc5f37f9f3affdf05046bf10a683b11a42
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 18:33:22 +0200

    update25

--- Project_sitomassaggi/Backend/API/Models/database.php
+++ Project_sitomassaggi/Backend/API/Models/database.php
@@ -16,8 +16,7 @@ class database extends \PDO
         $username = getenv('DB_USER');
         $password = getenv('DB_PASSWORD');
         try {
-            $cert = __DIR__ . "/ca.pem";
-            $this->conn= new PDO("pgsql:host=$serverName;port=17864;dbname=$database;user=$username;password=$password;sslmode=verify-ca;sslrootcert=$cert");
+            $this->conn= new PDO("pgsql:host=$serverName;port=17864;dbname=$database;user=$username;password=$password;sslmode=verify-ca;sslrootcert=/API/Models/ca.pem");
             $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             http_response_code(500);
