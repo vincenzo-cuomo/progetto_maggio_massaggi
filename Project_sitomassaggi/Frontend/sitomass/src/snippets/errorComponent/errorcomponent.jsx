0d922567239886b4c4b1e3b31d422e9ef27e7dce
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 10:57:25 +0200

    update

--- Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
+++ Project_sitomassaggi/Frontend/sitomass/src/snippets/errorComponent/errorcomponent.jsx
@@ -1,11 +1,10 @@
 import styles from "./errorcomponent.module.css"
-import sadFace from "../../../public/Images/sad.png"
 
 export default function ErrorComponent(){
     return (
         <div className={styles.errorMessage}> 
             Non è stato possibile ricavare i massaggi, riprovare più tardi
-            <img src={sadFace} style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
+            <img src="%PUBLIC_URL%/images/sad.png" style={{ width: "clamp(30px, 30%, 200px)", backgroundColor: "white", borderRadius: "50%", objectFit: "cover", marginLeft: "1%" }}></img>
         </div>
     )
 }
