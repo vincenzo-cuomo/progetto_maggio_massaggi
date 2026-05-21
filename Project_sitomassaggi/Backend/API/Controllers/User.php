69828e6e81e207473dc5f31d17e6f9ea203dd913
vincenzo-cuomo <vincenzocuomo123@icloud.com>
2026-05-21 19:32:05 +0200

    update34!

--- Project_sitomassaggi/Backend/API/Controllers/User.php
+++ Project_sitomassaggi/Backend/API/Controllers/User.php
@@ -39,48 +39,40 @@ class User
     function verifyLogin(string $email, string $password)
     {
         header("Access-Control-Expose-Headers: Authorization");
-        $sql = "SELECT PASSWORDUSER, IDUTENTE, UpdatedAt FROM userAccount WHERE EMAIL = :email";
 
-        $db = database::getInstance();
-        if ($this->db) {
-            try {
-                $stmt = $this->db->prepare($sql);
-                $stmt->bindValue(':email', $email);
-                $stmt->execute();
-            } catch (\PDOException $e) {
-                http_response_code(400);
-                header("Content-Type: application/json");
-                echo json_encode(["success" => false, "description" => "Sono state inserite una passwordd o mail sbagliata", "error" => $e->getMessage()]);
-                exit;
-            }
-
+        try {
+            $sql = "SELECT PASSWORDUSER, IDUTENTE, UpdatedAt FROM userAccount WHERE EMAIL = :email";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindValue(':email', $email);
+            $stmt->execute();
             $rows = $stmt->fetch(\PDO::FETCH_ASSOC);
-            if ($rows) {
-                if (password_verify(htmlspecialchars($password), $rows["PASSWORDUSER"])) {
-                    $jwt = new jwt;
-                    $userId = $rows['IDUTENTE'];
-                    $payload = ["sub" => $userId, "iat" => time(), "exp" => time() + 3600]; #jwt payload
-                    http_response_code(200);
-                    header("Content-Type: application/json");
-                    header("Cache-Control: no-cache, private");
-                    header("Authorization: Bearer " . $jwt->jwtCreate($payload));
-                    echo json_encode(["success" => true]);
-                } else {
-                    http_response_code(400);
-                    header("Content-Type: application/json");
-                    echo json_encode(["success" => false, "description" => "Sono state inserite una password o mail sbagliata"]);
-                    exit();
-                }
+        } catch (\PDOException $e) {
+            http_response_code(400);
+            header("Content-Type: application/json");
+            echo json_encode(["success" => false, "description" => "Sono state inserite una passwordd o mail sbagliata", "error" => $e->getMessage()]);
+            exit;
+        }
+
+        if ($rows) {
+            if (password_verify(htmlspecialchars($password), $rows["PASSWORDUSER"])) {
+                $jwt = new jwt;
+                $userId = $rows['IDUTENTE'];
+                $payload = ["sub" => $userId, "iat" => time(), "exp" => time() + 3600]; #jwt payload
+                http_response_code(200);
+                header("Content-Type: application/json");
+                header("Cache-Control: no-cache, private");
+                header("Authorization: Bearer " . $jwt->jwtCreate($payload));
+                echo json_encode(["success" => true]);
             } else {
                 http_response_code(400);
                 header("Content-Type: application/json");
                 echo json_encode(["success" => false, "description" => "Sono state inserite una password o mail sbagliata"]);
-                exit;
+                exit();
             }
         } else {
-            http_response_code(500);
+            http_response_code(400);
             header("Content-Type: application/json");
-            echo json_encode(["success" => false, "description" => "Connessione al DB fallita, riprovare più tardi"]);
+            echo json_encode(["success" => false, "description" => "Sono state inserite una password o mail sbagliata"]);
             exit;
         }
     }
